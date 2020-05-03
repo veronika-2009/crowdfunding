32880f76b714cab7892e21e2c810f3936ab3f543
Veronika <felizfeliz0808@gmail.com>
2020-05-03 12:54:40 +0300

    server db heroku

--- server.js
+++ server.js
@@ -11,9 +11,8 @@ const fileUpload = require('express-fileupload');
 
 var db = {}
 
-
-var sequelize = new Sequelize('users', 'root', 'mc1982118', {
-    host: 'localhost',
+var sequelize = new Sequelize('heroku_3d322cb1144a021', 'b7dd8f543e5f58', '5833cb15', {
+    host: 'us-cdbr-iron-east-01.cleardb.net',
     dialect: 'mysql',
     operatorsAliases: false,
     pool: {
